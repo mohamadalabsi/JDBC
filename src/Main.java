921f10082d2b3ee17aca47b57240d962cdffbc89
Mohammad Al Absi <absimoda1@gmail.com>
2024-08-28 23:58:08 +0200

    first connection

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String sql = "select emp_id from employee";
+        String url = "jdbc:postgresql://localhost/employee";
+        Str
+
+
+    }
+}

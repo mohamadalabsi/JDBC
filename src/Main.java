4481ebcd7e27ff6bc74ee54260ee3bb6ec9b308f
Mohammad Al Absi <absimoda1@gmail.com>
2024-08-29 00:53:13 +0200

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.sql.*;
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         String sql = "select emp_id from employee";
-        String url = "jdbc:postgresql://localhost/employee";
-        Str
+        String url = "jdbc:postgresql://localhost:5432/test";
+        Demo demo = new Demo();
 
+
+
+        Connection con = DriverManager.getConnection(url,demo.username, demo.password);
+        Statement stmt = con.createStatement();
+        ResultSet rs =  stmt.executeQuery(sql);
+
+        while(rs.next()){
+            int emp_id = rs.getInt("emp_id");
+            System.out.println(emp_id);
+        }
 
     }
 }
